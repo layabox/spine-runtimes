8e6dacb19ae055a4c9b4810f50c0f5b06e43cde0
jsjgameer <411963337@qq.com>
2024-05-31 14:12:13 +0800

    build

--- build-release.sh
+++ build-release.sh
@@ -2,12 +2,12 @@
 clear
 mkdir -p ./build
 mkdir -p dist
-cmake ./spine-laya  -B ./build/debug  -G "MinGW Makefiles" -DVERSION=wasm -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./dist  -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
-cd ./build/debug
+cmake ./spine-laya  -B ./build/release  -G "MinGW Makefiles" -DVERSION=wasm -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./dist  -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
+cd ./build/release
 emmake make -j4
 cd ../../dist
-cp ../build/debug/Spine.js .
-cp ../build/debug/Spine.wasm .
+cp ../build/release/Spine.js .
+cp ../build/release/Spine.wasm .
 
 
 sed -i "s/Spine.wasm/spine.wasm_3.8.wasm/g" Spine.js
@@ -17,12 +17,12 @@ mv Spine.js spine.wasm_3.8.js
 
 cd ../
 
-cmake ./spine-laya  -B ./build/debug  -G "MinGW Makefiles" -DVERSION=js -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./dist  -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
-cd ./build/debug
+cmake ./spine-laya  -B ./build/release  -G "MinGW Makefiles" -DVERSION=js -DTARGET_BUILD_PLATFORM=emscripten -DCMAKE_TOOLCHAIN_FILE=~/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake --no-warn-unused-cli -DCMAKE_INSTALL_PREFIX=./dist  -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_CROSSCOMPILING_EMULATOR=/home/ubuntu/emsdk/node/16.20.0_64bit/bin/node
+cd ./build/release
 emmake make -j4
 cd ../../dist
-cp ../build/debug/Spine.js .
-cp ../build/debug/Spine.js.mem .
+cp ../build/release/Spine.js .
+cp ../build/release/Spine.js.mem .
 
 sed -i "s/Spine.js.mem/spine_3.8.js.mem/g" Spine.js
 
